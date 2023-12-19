e74e8f51b1ad7beece3cd6319674b74a4df467d9
qwerty3248 <jesuslosada4484@gmail.com>
2023-12-19 20:57:07 +0100

    P4

---
+++ practica4/ejecutivo2.cpp
@@ -0,0 +1,101 @@
+// -----------------------------------------------------------------------------
+//
+// Sistemas concurrentes y Distribuidos.
+// Práctica 4. Implementación de sistemas de tiempo real.
+//
+// Archivo: ejecutivo2.cpp
+// Implementación del primer ejemplo de ejecutivo cíclico:
+//
+//   Datos de las tareas:
+//   ------------
+//   Ta.  T    C
+//   ------------
+//   A   500  100
+//   B   500  150
+//   C  1000  200
+//   D  2000  240
+//  -------------
+//
+//  Planificación (con Ts == 500 ms)
+//  *---------*----------*---------*--------*
+//  | A B C   | A B D    | A B C   | A B    |
+//  *---------*----------*---------*--------*
+//
+//
+// Historial:
+// Creado en Diciembre de 2017
+// -----------------------------------------------------------------------------
+
+#include <string>
+#include <iostream> // cout, cerr
+#include <thread>
+#include <chrono>   // utilidades de tiempo
+#include <ratio>    // std::ratio_divide
+
+using namespace std ;
+using namespace std::chrono ;
+using namespace std::this_thread ;
+
+// tipo para duraciones en segundos y milisegundos, en coma flotante:
+//typedef duration<float,ratio<1,1>>    seconds_f ;
+typedef duration<float,ratio<1,1000>> milliseconds_f ;
+
+// -----------------------------------------------------------------------------
+// tarea genérica: duerme durante un intervalo de tiempo (de determinada duración)
+
+void Tarea( const std::string & nombre, milliseconds tcomputo )
+{
+   cout << "   Comienza tarea " << nombre << " (C == " << tcomputo.count() << " ms.) ... " ;
+   sleep_for( tcomputo );
+   cout << "fin." << endl ;
+}
+
+// -----------------------------------------------------------------------------
+// tareas concretas del problema:
+
+void TareaA() { Tarea( "A", milliseconds(100) );  }
+void TareaB() { Tarea( "B", milliseconds( 150) );  }
+void TareaC() { Tarea( "C", milliseconds( 200) );  }
+void TareaD() { Tarea( "D", milliseconds( 250) );  }
+
+
+// -----------------------------------------------------------------------------
+// implementación del ejecutivo cíclico:
+
+int main( int argc, char *argv[] )
+{
+   // Ts = duración del ciclo secundario (en unidades de milisegundos, enteros)
+   const milliseconds Ts_ms(500);
+
+   // ini_sec = instante de inicio de la iteración actual del ciclo secundario
+   time_point<steady_clock> ini_sec = steady_clock::now();
+
+   while( true ) // ciclo principal
+   {
+      cout << endl
+           << "---------------------------------------" << endl
+           << "Comienza iteración del ciclo principal." << endl ;
+
+      for( int i = 1 ; i <= 4 ; i++ ) // ciclo secundario (4 iteraciones)
+      {
+      	time_point<steady_clock> instante1 = steady_clock::now();
+         cout << endl << "Comienza iteración " << i << " del ciclo secundario." << endl ;
+		
+         switch( i )
+         {
+            case 1 : TareaA(); TareaB(); TareaC();           break ;
+            case 2 : TareaA(); TareaB(); TareaD();           break ;
+            case 3 : TareaA(); TareaB(); TareaC();	     break ;
+            case 4 : TareaA(); TareaB();	             break ;
+         }
+
+         // calcular el siguiente instante de inicio del ciclo secundario
+         ini_sec += Ts_ms ;
+         // esperar hasta el inicio de la siguiente iteración del ciclo secundario
+         sleep_until( ini_sec );
+         time_point<steady_clock> instante2 = steady_clock::now();
+         float tiempo = milliseconds_f(instante2-instante1).count();
+         cout<<"El caso "<<i<<" ha tardado "<< tiempo<<" milisegundos, lo esperado es 500"<<endl;
+      }
+   }
+}
